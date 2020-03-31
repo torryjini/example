a2f7f82cd7797449d1805d2e8f8a7c9ff695ed43
torryjini <torryjini@users.noreply.github.com>
2020-03-31 22:41:23 +0900

    Add files via upload

--- script.js
+++ script.js
@@ -43,3 +43,25 @@
       document.getElementById("mySidebar").style.display = "none";
       document.getElementById("myOverlay").style.display = "none";
     }
+
+    //Get the button
+    var mybutton = document.getElementById("TopBtn");
+
+    // When the user scrolls down 30px from the top of the document, show the button
+    window.onscroll = function() {
+      scrollFunction()
+    };
+
+    function scrollFunction() {
+      if (document.body.scrollTop > 30 || document.documentElement.scrollTop > 30) {
+        mybutton.style.display = "block";
+      } else {
+        mybutton.style.display = "none";
+      }
+    }
+
+    // When the user clicks on the button, scroll to the top of the document
+    function topFunction() {
+      document.body.scrollTop = 0;
+      document.documentElement.scrollTop = 0;
+    } 
