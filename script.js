dc369a1ba4055084b2151943d4329ef7870ce9a2
torryjini <torryjini@users.noreply.github.com>
2020-06-24 17:20:24 +0900

    Add files via upload

--- script.js
+++ script.js
@@ -1,67 +1,79 @@
-    function myAccFunc() {
-      var x = document.getElementById("standard");
-      if (x.className.indexOf("w3-show") == -1) {
-        x.className += " w3-show";
-        x.previousElementSibling.className += "";
-      } else {
-        x.className = x.className.replace(" w3-show", "");
-        x.previousElementSibling.className =
-          x.previousElementSibling.className.replace("", "");
-      }
-    }
+function AccFunc(id) {
+  var x = document.getElementById(id);
+  if (x.className.indexOf("w3-show") == -1) {
+    x.className += " w3-show";
+    x.previousElementSibling.className += "";
+  } else {
+    x.className = x.className.replace(" w3-show", "");
+    x.previousElementSibling.className =
+      x.previousElementSibling.className.replace("", "");
+  }
+}
+//
+// function myAccFunc() {
+//   var x = document.getElementById("standard");
+//   if (x.className.indexOf("w3-show") == -1) {
+//     x.className += " w3-show";
+//     x.previousElementSibling.className += "";
+//   } else {
+//     x.className = x.className.replace(" w3-show", "");
+//     x.previousElementSibling.className =
+//       x.previousElementSibling.className.replace("", "");
+//   }
+// }
+//
+// function myAccFunc2() {
+//   var x = document.getElementById("etc");
+//   if (x.className.indexOf("w3-show") == -1) {
+//     x.className += " w3-show";
+//     x.previousElementSibling.className += "";
+//   } else {
+//     x.className = x.className.replace(" w3-show", "");
+//     x.previousElementSibling.className =
+//       x.previousElementSibling.className.replace("", "");
+//   }
+// }
+//
+// function myAccFunc3() {
+//   var x = document.getElementById("relsite");
+//   if (x.className.indexOf("w3-show") == -1) {
+//     x.className += " w3-show";
+//     x.previousElementSibling.className += "";
+//   } else {
+//     x.className = x.className.replace(" w3-show", "");
+//     x.previousElementSibling.className =
+//       x.previousElementSibling.className.replace("", "");
+//   }
+// }
 
-    function myAccFunc2() {
-      var x = document.getElementById("etc");
-      if (x.className.indexOf("w3-show") == -1) {
-        x.className += " w3-show";
-        x.previousElementSibling.className += "";
-      } else {
-        x.className = x.className.replace(" w3-show", "");
-        x.previousElementSibling.className =
-          x.previousElementSibling.className.replace("", "");
-      }
-    }
+function w3_open() {
+  document.getElementById("mySidebar").style.display = "block";
+  document.getElementById("myOverlay").style.display = "block";
+}
 
-    function myAccFunc3() {
-      var x = document.getElementById("relsite");
-      if (x.className.indexOf("w3-show") == -1) {
-        x.className += " w3-show";
-        x.previousElementSibling.className += "";
-      } else {
-        x.className = x.className.replace(" w3-show", "");
-        x.previousElementSibling.className =
-          x.previousElementSibling.className.replace("", "");
-      }
-    }
+function w3_close() {
+  document.getElementById("mySidebar").style.display = "none";
+  document.getElementById("myOverlay").style.display = "none";
+}
 
-    function w3_open() {
-      document.getElementById("mySidebar").style.display = "block";
-      document.getElementById("myOverlay").style.display = "block";
-    }
+//Get the button
+var mybutton = document.getElementById("TopBtn");
 
-    function w3_close() {
-      document.getElementById("mySidebar").style.display = "none";
-      document.getElementById("myOverlay").style.display = "none";
-    }
+// When the user scrolls down 10px from the top of the document, show the button
+window.onscroll = function() {
+  scrollFunction()
+};
 
-    //Get the button
-    var mybutton = document.getElementById("TopBtn");
+function scrollFunction() {
+  if (document.body.scrollTop > 10 || document.documentElement.scrollTop > 10) {
+    mybutton.style.display = "block";
+  } else {
+    mybutton.style.display = "none";
+  }
+}
 
-    // When the user scrolls down 10px from the top of the document, show the button
-    window.onscroll = function() {
-      scrollFunction()
-    };
-
-    function scrollFunction() {
-      if (document.body.scrollTop > 10 || document.documentElement.scrollTop > 10) {
-        mybutton.style.display = "block";
-      } else {
-        mybutton.style.display = "none";
-      }
-    }
-
-    // When the user clicks on the button, scroll to the top of the document
-    function topFunction() {
-      document.body.scrollTop = 0;
-      document.documentElement.scrollTop = 0;
-    }
+// When the user clicks on the button, scroll to the top of the document
+function topFunction() {
+  document.body.scrollTop = 0;
+  document.documentElement.scrollTop = 0;
+}
